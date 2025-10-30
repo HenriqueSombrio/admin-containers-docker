8c287a85b4de027e1870145155f0884cabdf978c
Henrique Sombrio <33074259+HenriqueSombrio@users.noreply.github.com>
2025-10-30 10:57:21 -0300

    Fix CORS removido comentários

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Em produção, defina a origem do seu frontend
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
